00752293fe83855e1a6902dbfca85b020a310fec
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2018-03-23 19:55:55 +0700

    Manufacture - list, and fixing code

--- resources/views/admin/manufacturing/index.blade.php
+++ resources/views/admin/manufacturing/index.blade.php
@@ -18,7 +18,52 @@
   <section class="content">
     <div class="row">
       <div class="col-md-12">
-        <h1>Manufacturing</h1>
+        <div class="box">
+          <div class="box-header clearfix">
+            <h3 class="box-title pull-left">List Order</h3>
+            <div class="pull-right">
+              <a href="#">
+                <div class="btn btn-primary">
+                  Create New
+                </div>
+              </a>
+            </div>
+          </div>
+
+          <div class="box-body">
+            <table class="table table-borderless" id="datatable-manufacturing">
+              <thead>
+                <tr>
+                  <th class="text-center">Name</th>
+                  <th class="text-center">Quantity to Produce</th>
+                  <th class="text-center">Bill of materials</th> 
+                  <th class="text-center">Deadline</th> 
+                  <th class="text-center">Responsible</th>
+                  <th class="text-center">Actions</th>
+                </tr>
+              </thead>
+              <tbody id="item">
+              {{-- @foreach($manufactures as $item)
+                <tr class="item{{$item->id}}">
+                  <td>{{$item->name}}</td>
+                  <td>{{$item->email}}</td>
+                  <td>{{$item->role}}</td>
+                  <td>{{$item->status}}</td>
+                  <td>
+                    <button class="edit-modal btn btn-info" data-info="{{$item->id}},{{$item->name}},{{$item->email}},{{$item->role}},{{$item->status}}">
+                      <span class="glyphicon glyphicon-edit"></span> Edit
+                    </button>
+                    <button class="delete-modal btn btn-danger"
+                      data-info="{{$item->id}},{{$item->name}},{{$item->email}},{{$item->role}},{{$item->status}}">
+                      <span class="glyphicon glyphicon-trash"></span> Delete
+                    </button></td>
+                </tr>
+              @endforeach --}}
+              </tbody>
+            </table>
+          </div>
+
+        </div>
       </div>
     </div>
   </section>
@@ -29,5 +74,9 @@
 @endpush
 
 @push('scripts')
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('#datatable-manufacturing').DataTable();
+    });
 @endpush
 
