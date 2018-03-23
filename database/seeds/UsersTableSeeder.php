344c6ef54816bacb57aa1eabd2cf0a55fea96742
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2018-03-23 13:38:10 +0700

    Seeder - update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
 use App\User;
+
 class UsersTableSeeder extends Seeder
 {
     /**
@@ -13,14 +14,21 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->truncate();
-        User::create([
-            'name' => 'Marvelous',
-            'email' => 'admin@admin.com',
-            'password' => bcrypt('Today1234'),
-            'status' => '1',
-            'image' => 'admin.png',
-            'created_at' => Carbon::now(),
-        ]);
+
+        $user = new User();
+        $user->name = 'Marvelous';
+        $user->email = 'admin@admin.com';
+        $user->password = bcrypt('Today1234');
+        $user->status = true;
+        $user->image = 'admin.png';
+        $user->save();
         
+        $user = new User();
+        $user->name = 'Maulana Yusup';
+        $user->email = 'maulanayusupp@gmail.com';
+        $user->password = bcrypt('admin');
+        $user->status = true;
+        $user->image = 'admin.png';
+        $user->save();
     }
 }
