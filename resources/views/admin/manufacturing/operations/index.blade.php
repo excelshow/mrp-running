19339b450d81030ab089979f01b5d81f2fc658b7
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2018-03-24 18:15:32 +0700

    Update operations menu

--- resources/views/admin/manufacturing/operations/index.blade.php
+++ resources/views/admin/manufacturing/operations/index.blade.php
@@ -18,33 +18,34 @@
     <div class="row">
       <div class="col-lg-12 col-xs-12">
         <div class="small-box bg-aqua min">
-          <div class="inner">
-            <h3>Manufacturing Orders</h3>
+          <div class="box-menu" style="padding: 20px; text-align: center;">
+            <h1 style="font-weight: bold;">Manufacturing Orders</h1>
+            <div class="icon">
+              <i class="fa fa-file-image-o"></i>
+            </div>  
           </div>
-          <div class="icon">
-            <i class="fa fa-file-image-o"></i>
-          </div>
+          
           <a href="{{ url('manufacturing/operations/manufacturing-orders') }}" class="small-box-footer">Enter <i class="fa fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <div class="col-lg-12 col-xs-12">
         <div class="small-box bg-green min">
-          <div class="inner">
-            <h3>Unbuild Orders</h3>
-          </div>
-          <div class="icon">
-            <i class="fa fa-smile-o"></i>
+          <div class="box-menu" style="padding: 20px; text-align: center;">
+            <h1 style="font-weight: bold;">Unbuild Orders</h1>
+            <div class="icon">
+              <i class="fa fa-smile-o"></i>
+            </div>
           </div>
           <a href="{{ url('manufacturing/operations/unbuild-orders') }}" class="small-box-footer">Enter <i class="fa fa-arrow-circle-right"></i></a>
         </div>
       </div>
       <div class="col-lg-12 col-xs-12">
         <div class="small-box bg-yellow min">
-          <div class="inner">
-            <h3>Scrap Orders</h3>
-          </div>
-          <div class="icon">
-            <i class="ion ion-person-add"></i>
+          <div class="box-menu" style="padding: 20px; text-align: center;">
+            <h1 style="font-weight: bold;">Scrap Orders</h1>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
           </div>
           <a href="{{ url('manufacturing/operations/scrap-orders') }}" class="small-box-footer">Enter <i class="fa fa-arrow-circle-right"></i></a>
         </div>
