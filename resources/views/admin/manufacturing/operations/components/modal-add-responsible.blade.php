575072a84eb49273cf44f66ec3be806428b65116
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2018-03-25 13:36:18 +0700

    Fixing modal add responsible

--- resources/views/admin/manufacturing/operations/components/modal-add-responsible.blade.php
+++ resources/views/admin/manufacturing/operations/components/modal-add-responsible.blade.php
@@ -4,7 +4,7 @@
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title">Add Product</h4>
+        <h4 class="modal-title">Add Responsible</h4>
       </div>
       <div class="modal-body">
         <form class="form-horizontal" action="" method="post">
