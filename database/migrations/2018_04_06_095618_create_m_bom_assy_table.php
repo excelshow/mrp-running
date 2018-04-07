5e940df5247a0156999296a17479405c0d044874
Maulana Yusup Abdullah <maulanayusupp@gmail.com>
2018-04-07 15:19:33 +0700

    DB - Migration EBOM MBOM

--- database/migrations/2018_04_06_095618_create_m_bom_assy_table.php
+++ database/migrations/2018_04_06_095618_create_m_bom_assy_table.php
@@ -15,6 +15,19 @@ class CreateMBomAssyTable extends Migration
     {
         Schema::create('m-bom-assy', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('master_material_id')->nullable();
+            $table->integer('part_level')->nullable();
+            $table->string('part_nha')->nullable();
+            $table->string('part_name')->nullable();
+            $table->integer('quantity_part')->nullable();
+            $table->string('part_uom')->nullable();
+            $table->string('part_type')->nullable();
+            $table->string('part_symbol')->nullable();
+            $table->integer('part_changes')->nullable();
+            $table->boolean('is_approved')->nullable()->default(false);
+            $table->string('approved_date')->nullable();
+            $table->integer('user_id_created')->nullable();
+            $table->integer('user_id_updated')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2018_04_06_095644_create_e_bom_single_parts_table.php
+++ database/migrations/2018_04_06_095644_create_e_bom_single_parts_table.php
@@ -15,6 +15,34 @@ class CreateEBomSinglePartsTable extends Migration
     {
         Schema::create('e-bom-single-parts', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('e_bom_assy_id')->nullable();
+            $table->integer('part_level')->nullable();
+            $table->string('part_number')->nullable();
+            $table->string('part_nha')->nullable();
+            $table->string('part_name')->nullable();
+            $table->integer('quantity_part_per_sheet')->nullable();
+            $table->string('part_uom')->nullable();
+            $table->string('part_type')->nullable();
+            $table->integer('koding_id')->nullable();
+            $table->string('make')->nullable();
+            $table->string('part_symbol')->nullable();
+            $table->integer('part_changes')->nullable();
+            $table->integer('part_material_id')->nullable();
+            $table->string('part_serial_number')->nullable();
+            $table->decimal('quantity_part_thickness')->nullable();
+            $table->decimal('quantity_part_width')->nullable();
+            $table->decimal('quantity_part_length')->nullable();
+            $table->decimal('quantity_part_diameter')->nullable();
+            $table->decimal('quantity_part_weight')->nullable();
+            $table->string('material_specification')->nullable();
+            $table->decimal('quantity_material')->nullable();
+            $table->decimal('quantity_kg_pcs')->nullable();
+            $table->decimal('part_ratio')->nullable();
+            $table->decimal('dim_uom')->nullable();
+            $table->string('weight_uom')->nullable();
+            $table->string('remark')->nullable();
+            $table->integer('user_id_created')->nullable();
+            $table->integer('user_id_updated')->nullable();
             $table->timestamps();
         });
     }
